f886e803093e9c0c82aa15b1717e31275daf1f49
jemeetala <jemeetala@gmail.com>
2022-08-26 15:01:24 +0530

    inital commit

---
+++ src/service/api.js
@@ -0,0 +1,105 @@
+import { apis } from "service";
+
+const COMMON_URL = `https://`;
+
+const API_URLS = {
+  GET_BRANCHES: `${COMMON_URL}dhiwise-team.freshteam.com/api/branches`,
+  GET_TEAMS: `${COMMON_URL}dhiwise-team.freshteam.com/api/teams`,
+
+  GET_EMPLOYEECAPABILITYSELECT: `${COMMON_URL}rpspezcanqopwgztzymx.supabase.co/rest/v1/Employee_capability`,
+  GET_EMPLOYEECAPABILITYIDEQ5: `${COMMON_URL}rpspezcanqopwgztzymx.supabase.co/rest/v1/Employee_capability`,
+  PATCH_EMPLOYEECAPABILITYIDEQ5: `${COMMON_URL}rpspezcanqopwgztzymx.supabase.co/rest/v1/Employee_capability`,
+  POST_EMPLOYEECAPABILITY: `${COMMON_URL}rpspezcanqopwgztzymx.supabase.co/rest/v1/Employee_capability`,
+  GET_EMPLOYEES: `${COMMON_URL}dhiwise-team.freshteam.com/api/employees`,
+};
+
+export const getBranches = (payload) =>
+  apis.get(API_URLS.GET_BRANCHES, {
+    ...payload,
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer VCcK26hV70FYHIsD2h2spA",
+      "content-type": "application/json",
+      ...payload?.headers,
+    },
+  });
+
+  export const getTeams = (payload) =>
+  apis.get(API_URLS.GET_TEAMS, {
+    ...payload,
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer VCcK26hV70FYHIsD2h2spA",
+      "content-type": "application/json",
+      ...payload?.headers,
+    },
+  });
+
+export const getEmployeecapabilityselect = (payload) =>
+  apis.get(API_URLS.GET_EMPLOYEECAPABILITYSELECT, {
+    ...payload,
+    params: { select: "*", ...payload?.params },
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwc3BlemNhbnFvcHdnenR6eW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTczNDgwNTksImV4cCI6MTk3MjkyNDA1OX0.xcmHxepkSt8oG5-a3vr8R8vB4rVxxqL_yS8il1HwDEY",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwc3BlemNhbnFvcHdnenR6eW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTczNDgwNTksImV4cCI6MTk3MjkyNDA1OX0.xcmHxepkSt8oG5-a3vr8R8vB4rVxxqL_yS8il1HwDEY",
+      Range: "0-9",
+      "content-type": "application/json",
+      ...payload?.headers,
+    },
+  });
+  export const getEmployeecapabilityideq5 = (payload) => 
+  apis.get(API_URLS.GET_EMPLOYEECAPABILITYIDEQ5, {
+    ...payload,
+    params: { id: `eq.${payload?.params.id}` },
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwc3BlemNhbnFvcHdnenR6eW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTczNDgwNTksImV4cCI6MTk3MjkyNDA1OX0.xcmHxepkSt8oG5-a3vr8R8vB4rVxxqL_yS8il1HwDEY",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwc3BlemNhbnFvcHdnenR6eW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTczNDgwNTksImV4cCI6MTk3MjkyNDA1OX0.xcmHxepkSt8oG5-a3vr8R8vB4rVxxqL_yS8il1HwDEY",
+      Range: "0-9",
+      "content-type": "application/json",
+      ...payload?.headers,
+    },
+  });
+
+export const patchEmployeecapabilityideq5 = (payload) =>
+  apis.patch(API_URLS.PATCH_EMPLOYEECAPABILITYIDEQ5, {
+    ...payload,
+    params: { id: `eq.${payload?.params.id}` },
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwc3BlemNhbnFvcHdnenR6eW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTczNDgwNTksImV4cCI6MTk3MjkyNDA1OX0.xcmHxepkSt8oG5-a3vr8R8vB4rVxxqL_yS8il1HwDEY",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwc3BlemNhbnFvcHdnenR6eW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTczNDgwNTksImV4cCI6MTk3MjkyNDA1OX0.xcmHxepkSt8oG5-a3vr8R8vB4rVxxqL_yS8il1HwDEY",
+      "Content-Type": "application/json",
+      Prefer: "return=representation",
+      "content-type": "application/json",
+      ...payload?.headers,
+    },
+  });
+
+export const postEmployeecapability = (payload) =>
+  apis.post(API_URLS.POST_EMPLOYEECAPABILITY, {
+    ...payload,
+    headers: {
+      apikey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwc3BlemNhbnFvcHdnenR6eW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTczNDgwNTksImV4cCI6MTk3MjkyNDA1OX0.xcmHxepkSt8oG5-a3vr8R8vB4rVxxqL_yS8il1HwDEY",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwc3BlemNhbnFvcHdnenR6eW14Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTczNDgwNTksImV4cCI6MTk3MjkyNDA1OX0.xcmHxepkSt8oG5-a3vr8R8vB4rVxxqL_yS8il1HwDEY",
+      "Content-Type": "application/json",
+      ...payload?.headers,
+    },
+  });
+
+export const getEmployees = (payload) =>
+  apis.get(API_URLS.GET_EMPLOYEES, {
+    ...payload,
+    headers: {
+      accept: "application/json",
+      Authorization: "Bearer VCcK26hV70FYHIsD2h2spA",
+      "content-type": "application/json",
+      ...payload?.headers,
+    },
+  });
